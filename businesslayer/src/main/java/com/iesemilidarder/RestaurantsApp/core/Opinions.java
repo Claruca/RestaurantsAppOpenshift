9140b10b5de2b3f330e2bb0ab36a0fd3d5ef2cb0
claruca <cmartialberti@gmail.com>
2018-01-19 17:48:18 +0100

    api-rest

---
+++ businesslayer/src/main/java/com/iesemilidarder/RestaurantsApp/core/Opinions.java
@@ -0,0 +1,40 @@
+package com.iesemilidarder.restaurants.web;
+
+public class Opinions {
+    private String opicodi;
+    private String observacio;
+    private String puntuacio;
+    private String opirevisada;
+
+    public String getOpicodi() {
+        return opicodi;
+    }
+
+    public void setOpicodi(String opicodi) {
+        this.opicodi = opicodi;
+    }
+
+    public String getObservacio() {
+        return observacio;
+    }
+
+    public void setObservacio(String observacio) {
+        this.observacio = observacio;
+    }
+
+    public String getPuntuacio() {
+        return puntuacio;
+    }
+
+    public void setPuntuacio(String puntuacio) {
+        this.puntuacio = puntuacio;
+    }
+
+    public String getOpirevisada() {
+        return opirevisada;
+    }
+
+    public void setOpirevisada(String opirevisada) {
+        this.opirevisada = opirevisada;
+    }
+}
