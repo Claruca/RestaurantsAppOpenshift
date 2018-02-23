793a3a4540c4a300222b04f87b6a054b80187004
Clara <cmartialberti@gmail.com>
2018-02-23 18:43:41 +0100

    con spring security

--- springrestapi/src/main/java/com/iesemilidarder/RestaurantsApp/controller/HomeController.java
+++ springrestapi/src/main/java/com/iesemilidarder/RestaurantsApp/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.util.List;
 
 
